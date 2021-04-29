574c374717ae687654c0f55825d9da682154881f
st97110 <47217662+st97110@users.noreply.github.com>
2021-04-29 23:48:14 +0800

    Update Integer.h

--- Integer.h
+++ Integer.h
@@ -1,5 +1,7 @@
-#pragma once
+#ifndef INTEGER_H
+#define INTEGER_H
 #include "Number.h"
+class Decimal;
 class Integer : public Number
 {
 public:
@@ -10,10 +12,8 @@ public:
 	Integer& operator=(Integer&); // A = 48763000 B = A
 	Integer& operator=(string&); // A = 487630000
 
-	Integer Power(Integer& left, Integer& right); // A ^ B
-	Integer Power(Integer& left, string& right); // A ^ 3
-	friend Integer Power(Integer& left, string& right); // 3 ^ A
-
+	// A = 3 
+	// PI = 3.14
 	friend Integer operator+(Integer&, Integer&); // A + B
 	friend Integer operator-(Integer&, Integer&); // A - B
 	friend Integer operator*(Integer&, Integer&); // A * B
@@ -29,8 +29,13 @@ public:
 	friend Integer operator*(string& right, Integer&); // 3 * A
 	friend Integer operator/(string& right, Integer&); // 3 / A		
 
-	friend Integer Factorial(Integer& a); // A!
-	friend Integer Factorial(string& a); // 5!
+	friend Integer Power(Integer& left, Integer& right); // A ^ B
+	friend Integer Power(Integer& left, string& right); // A ^ 3
+	friend Integer Power(Integer& left, string& right); // 3 ^ A
 
-	std::string ans; // 答案
+	friend Integer Factorial(Integer& a); // A!
+	friend Integer Factorial(string& a); // 5!  3.14! 小數階乘報錯
+
+	string ans; // 答案
 };
+#endif // !INTEGER_H
