0db695a3748390b9e097868795d591cb9b3c47f0
st97110 <47217662+st97110@users.noreply.github.com>
2021-05-02 22:34:54 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include "Number.h"
 int main()
 {
+	Number f("000020.0012123002");
+	cout << f.printAns() << endl;
+
+	cout << "0.";
+	for (int i = 0; i < 100; i++)
+	{
+		cout << "0";
+	}
+	Integer A = "---3";
+	vector<Number*> nums;
+	nums.push_back(&A);
+	for(const auto & num : nums)
+		cout << *num << endl;
+
 	cout << "Command : Set (Int/Dec/Com) / Print / Assign / Pow / Fac / or any expression." << endl;
 	cout << "-----------------------------------------------------------------------------" << endl;
 	while (1)
@@ -8,7 +22,7 @@ int main()
 		string in, order;
 		getline(cin, in);
 		Number k(in);
-		cout << "=============================================================================" << endl;
+		cout <<  endl << k.ans.numerator << endl << "=============================================================================" << endl;
 	}
 	return 0;
 }
