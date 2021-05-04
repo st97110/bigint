94d019bd53b1741c476c09a7932937190034909c
st97110 <47217662+st97110@users.noreply.github.com>
2021-05-04 12:47:20 +0800

    Update Integer.cpp

--- Integer.cpp
+++ Integer.cpp
@@ -4,28 +4,62 @@ using namespace std;
 //Constructor
 Integer::Integer() : Number()
 {
-	ans = "0";
+	intAns = getInt();
 }
 Integer::Integer(const char* input) : Number(input)
 {
-	ans = getInt();
+	intAns = getInt();
 }
 Integer::~Integer() {}
 
 void Integer::assign() // 繼承number
 {
-	ans = getInt();
+	intAns = getInt();
+}
+
+string Integer::getAns()const
+{
+	return intAns;
+}
+
+newString Integer::getFrac()const
+{
+	return ans;
+}
+
+string Integer::getInput()const
+{
+	return input;
 }
 
 void Integer::print(ostream& out) const // 繼承number
 {
-	string ret = ans;
+	string ret = intAns;
 	out << ret;
+}
+
+Integer& Integer::operator=(Integer& r)
+{
+	errorTyep = r.errorTyep;
+	ans = r.ans;
+	input = r.input;
+	assign();
+	return *this;
+}
+
+Integer& Integer::operator=(Decimal& r)
+{
+	errorTyep = r.errorTyep;
+	//ans = r.getFrac();     ///////////////////////////////
+	//input = r.getInput();
+	assign();
+	return *this;
 }
 
 Integer& Integer::operator=(string& r)
 {
 	setInput(r);
+	inToPostfix();
 	computInput();
 	assign();
 	return *this;
@@ -34,7 +68,78 @@ Integer& Integer::operator=(string& r)
 Integer operator+(Integer& left, Integer& right)
 {
 	Integer ret;
-	ret.setInput(left.ans + '+' + right.ans);
+	ret.setInput(left.getInt() + '+' + right.getInt());
+	ret.inToPostfix();
+	ret.computInput();
+	ret.assign();
+	return ret;
+}
+
+Integer operator-(Integer& left, Integer& right)
+{
+	Integer ret;
+	ret.setInput(left.getInt() + '-' + right.getInt());
+	ret.inToPostfix();
+	ret.computInput();
+	ret.assign();
+	return ret;
+}
+
+Integer operator*(Integer& left, Integer& right)
+{
+	Integer ret;
+	ret.setInput(left.getInt() + '*' + right.getInt());
+	ret.inToPostfix();
+	ret.computInput();
+	ret.assign();
+	return ret;
+}
+
+Integer operator/(Integer& left, Integer& right)
+{
+	Integer ret;
+	ret.setInput(left.getInt() + '/' + right.getInt());
+	ret.inToPostfix();
+	ret.computInput();
+	ret.assign();
+	return ret;
+}
+
+Integer operator+(Integer& left, Decimal& right)
+{
+	Integer ret;
+	ret.setInput(left.getInt() + '+' + right.getDec());
+	ret.inToPostfix();
+	ret.computInput();
+	ret.assign();
+	return ret;
+}
+
+Integer operator-(Integer& left, Decimal& right)
+{
+	Integer ret;
+	ret.setInput(left.getInt() + '-' + right.getDec());
+	ret.inToPostfix();
+	ret.computInput();
+	ret.assign();
+	return ret;
+}
+
+Integer operator*(Integer& left, Decimal& right)
+{
+	Integer ret;
+	ret.setInput(left.getInt() + '*' + right.getDec());
+	ret.inToPostfix();
+	ret.computInput();
+	ret.assign();
+	return ret;
+}
+
+Integer operator/(Integer& left, Decimal& right)
+{
+	Integer ret;
+	ret.setInput(left.getInt() + '/' + right.getDec());
+	ret.inToPostfix();
 	ret.computInput();
 	ret.assign();
 	return ret;
