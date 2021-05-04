97f3027c201c75106b63142447acaca2bbbf899b
st97110 <47217662+st97110@users.noreply.github.com>
2021-05-04 12:47:30 +0800

    Update Integer.h

--- Integer.h
+++ Integer.h
@@ -8,32 +8,25 @@ public:
 	Integer(const char*);
 	~Integer();
 
+	string getAns()const; // 繼承
+	newString getFrac()const; // 繼承
+	string getInput()const; // 繼承
 	virtual void assign(); // 繼承
 	virtual void print(ostream&)const; // 繼承
-	//Integer& operator=(Integer&); // A = 48763000 B = A
-	Integer& operator=(string&); // A = 487630000
+
+	Integer& operator=(Integer& rhs);
+	Integer& operator=(Decimal& rhs);
+	Integer& operator=(string& rhs);
 
 	friend Integer operator+(Integer&, Integer&); // A + B
-	//friend Integer operator-(Integer&, Integer&); // A - B
-	//friend Integer operator*(Integer&, Integer&); // A * B
-	//friend Integer operator/(Integer&, Integer&); // A / B
-	//
-	//friend Integer operator+(Integer&, string& right); // A + 3
-	//friend Integer operator-(Integer&, string& right); // A - 3
-	//friend Integer operator*(Integer&, string& right); // A * 3
-	//friend Integer operator/(Integer&, string& right); // A / 3
+	friend Integer operator-(Integer&, Integer&); // A - B
+	friend Integer operator*(Integer&, Integer&); // A * B
+	friend Integer operator/(Integer&, Integer&); // A / B
 
-	//friend Integer operator+(string& right, Integer&); // 3 + A
-	//friend Integer operator-(string& right, Integer&); // 3 - A
-	//friend Integer operator*(string& right, Integer&); // 3 * A
-	//friend Integer operator/(string& right, Integer&); // 3 / A		
+	friend Integer operator+(Integer&, Decimal&); // A + PI
+	friend Integer operator-(Integer&, Decimal&); // A - PI
+	friend Integer operator*(Integer&, Decimal&); // A * PI
+	friend Integer operator/(Integer&, Decimal&); // A / PI
 
-	//friend Integer Power(Integer& left, Integer& right); // A ^ B
-	//friend Integer Power(Integer& left, string& right); // A ^ 3
-	//friend Integer Power(Integer& left, string& right); // 3 ^ A
-
-	//friend Integer Factorial(Integer& a); // A!
-	//friend Integer Factorial(string& a); // 5!  3.14! 小數階乘報錯
-
-	string ans; // 答案
+	string intAns; // 答案
 };
